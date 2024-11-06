851f8650c14368219c9324f0ad0db6b6091e43a1
Daniellespaiva <daniellespaiva@outlook.com>
2024-11-06 00:37:33 -0300

    nova atualização

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,14 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Kalam-Bold.ttf", "KalamBold");
+                    fonts.AddFont("Kalam-Light.ttf", "KalamLight");
+                    fonts.AddFont("Kalam-Regular.ttf", "Kalam");
+                    fonts.AddFont("PTSerif-Bold.ttf", "PTSerifBold");
+                    fonts.AddFont("PTSerif-BoldItalic.ttf", "PTSerifBoldItalic");
+                    fonts.AddFont("PTSerif-Italic..ttf", "PTSerifItalic.");
+                    fonts.AddFont("PTSerif-Regular.ttf", "PTSerifRegular");
+
                 });
 
 #if DEBUG
---
+++ MauiAppHotel/Resources/Images/paisagem.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppHotel.Resources.Images;
+
+public partial class paisagem : ContentPage
+{
+	public paisagem()
+	{
+		InitializeComponent();
+	}
+}
